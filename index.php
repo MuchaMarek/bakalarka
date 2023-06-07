fb4fcec76b84f37e2d643ca7364bf596ce5cd4ff
Marchie <srotakm9@gmail.com>
2023-06-07 20:37:37 +0200

    Comparation works, but all line names on graph are the parameter they signify

--- index.php
+++ index.php
@@ -11,32 +11,18 @@ try {
 $sql = "SELECT DISTINCT sensor_id FROM sensor_data";
 $result = $db->query($sql);
 
+$sensors = array();
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     $sensors[] = $row['sensor_id'];
-}
-
-$desiredParams = [];
-$comparedSensors = [];
-$addressList = [];
-
-foreach ($sensors as $sensor) {
-    $sql = "SELECT city, address, country FROM sensor_data WHERE sensor_id = :sensor_id LIMIT 1";
-    $stmt = $db->prepare($sql);
-    $stmt->execute(['sensor_id' => $sensor]);
-    $address = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($address) {
-        $addressList[] = $address["city"] . " " . $address["address"] . ", " . $address["country"];
-    }
 }
 ?>
 
 <html lang="sk">
 <head>
     <title>MuchaData</title>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="/styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script>
@@ -52,7 +38,6 @@ foreach ($sensors as $sensor) {
             var period = document.querySelector('input[name="period"]:checked').value;
             var startDate = document.getElementById('start-date').value;
             var endDate = document.getElementById('end-date').value;
-
 
             if (period === "fromUntil" && ((startDate === "" || endDate === "") || (startDate > endDate))) {
                 document.getElementById('day').checked = false;
@@ -111,6 +96,87 @@ foreach ($sensors as $sensor) {
                     drawChart(dataArray, '<?php echo $param; ?> (' + unit + ')', 'chart_<?php echo $sensor . "_" . $param; ?>');
                     <?php endforeach; ?>
                     <?php endforeach; ?>
+                })
+                .catch(function (error) {
+                    console.log("Fetch error: " + error.message);
+                });
+        }
+
+
+        function makeCompareCharts(desiredParameters, comparedSensors) {
+            if (desiredParameters.length === 0 || comparedSensors.length === 0) {
+                return;
+            }
+            let sensorData = {};
+
+            let period = document.querySelector('input[name="period"]:checked').value;
+            let startDate = document.getElementById('start-date').value;
+            let endDate = document.getElementById('end-date').value;
+
+            if (period === "fromUntil" && ((startDate === "" || endDate === "") || (startDate > endDate))) {
+                document.getElementById('day').checked = false;
+                document.getElementById('week').checked = false;
+                document.getElementById('month').checked = false;
+                document.getElementById('fromUntil').checked = false;
+                document.getElementById('start-date').value = "";
+                document.getElementById('end-date').value = "";
+                return;
+            }
+
+            fetch('getSensorData.php?period=' + period + '&start-date=' + startDate + '&end-date=' + endDate)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error("HTTP error " + response.status);
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    sensorData = data;
+                    desiredParameters.forEach(function (param) {
+                        let unit = "";
+                        var completeData = [];
+                        var sensor = "";
+                        switch (param) {
+                            case 'temperature':
+                                unit = "C°";
+                                break;
+                            case 'humidity':
+                                unit = "%";
+                                break;
+                            case 'pressure':
+                                unit = "hPa";
+                                break;
+                            case 'pm25':
+                            case 'pm100':
+                                unit = "µg/m3";
+                                break;
+                        }
+                        comparedSensors.forEach(function (currentSensor) {
+                            sensor = currentSensor;
+                            var dataArray = sensorData[sensor][param];
+
+                            if (period === 'day') {
+                                for (let i = 1; i < dataArray.length; i++) {
+                                    dataArray[i][0] = parseInt(dataArray[i][0]);    // hour
+                                }
+                            } else if (period === 'week' || period === 'month' || period === 'fromUntil') {
+                                for (let i = 1; i < dataArray.length; i++) {
+                                    let dateParts = dataArray[i][0].split('-');
+                                    dataArray[i][0] = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
+                                }
+                            }
+
+                            if (completeData.length === 0) {
+                                completeData.push(...dataArray);
+                            } else {
+                                for (let i = 0; i < dataArray.length; i++) {
+                                    completeData[i].push(dataArray[i][1]);
+                                }
+                            }
+                            console.log(completeData);
+                        });
+                        drawChart(completeData, param + ' (' + unit + ')', 'chart_' + param);
+                    });
                 })
                 .catch(function (error) {
                     console.log("Fetch error: " + error.message);
@@ -231,7 +297,6 @@ foreach ($sensors as $sensor) {
                 drawCharts();
             });
 
-
             $('#clearBtn').click(function (e) {
                 e.preventDefault();
                 document.getElementById('start-date').value = "";
@@ -243,156 +308,159 @@ foreach ($sensors as $sensor) {
                 drawCharts();
             });
 
+            // COMPARE STUFF
+            var desiredParameters = [];
+            var comparedSensors = [];
 
             $('#compareBtn').click(function (e) {
                 e.preventDefault();
-                $('.sensor-row').hide();
+                desiredParameters = [];
+                comparedSensors = [];
+                document.getElementById("compareGraphsDiv").style.display = "block";
+                document.getElementById("defaultGraphsDiv").style.display = "none";
+                document.getElementById("popCompareMenu").style.display = "block";
                 document.getElementById("compareShow1").style.background = "green";
                 document.getElementById("compareShow2").style.background = "green";
-                drawCharts();
             });
-
 
             $('#clearCompareBtn').click(function (e) {
                 e.preventDefault();
-                $('.sensor-row').show();
+                document.getElementById("compareGraphsDiv").style.display = "none";
+                document.getElementById("defaultGraphsDiv").style.display = "block";
+                document.getElementById("popCompareMenu").style.display = "none";
+                document.getElementById("compareShow1").style.background = "red";
+                document.getElementById("compareShow2").style.background = "red";
+                makeCompareCharts(desiredParameters, comparedSensors);
+                desiredParameters.forEach(function (param) {  // Removed extra )
+                    var title = "chart_" + param;
+                    document.getElementById(title).style.display = "none";
+                });  // Added missing closing parenthesis
+                desiredParameters = [];
+                comparedSensors = [];
                 drawCharts();
             });
 
-            $('#temp').click(function (e) {
+
+            $("#addSensorBtn").click(function () {
+                var sensorIndex = document.getElementById("sensorIndex").value;
+                $.ajax({
+                    url: 'getSensor.php',
+                    type: 'POST',
+                    data: {'sensorIndex': sensorIndex},
+                    success: function (addedSensor) {
+                        if (!comparedSensors.includes(addedSensor)) {
+                            comparedSensors.push(addedSensor);
+                        }
+                    },
+                    error: function (err) {
+                        console.log("no sensor found");
+                    }
+                });
+            });
+
+            $('#compareSubmit').click(function (e) {
                 e.preventDefault();
-                <?php
-                $key = array_search("temperature", $desiredParams);
-                if ($key !== false) {
-                    unset($desiredParams[$key]);
-                } else {
-                    $desiredParams[] = "temperature";
-                }
-                ?>
-                drawCharts();
+                desiredParameters = [];
+                $('#compareDiv input:checked').each(function () {
+                    desiredParameters.push($(this).val().toLowerCase());
+                });
+                makeCompareCharts(desiredParameters, comparedSensors);
+                desiredParameters.forEach(function (param) {
+                    var title = "chart_" + param;
+                    document.getElementById(title).style.display = "block";
+                });
             });
 
-            $('#pres').click(function (e) {
-                e.preventDefault();
-                <?php
-                $key = array_search("pm25", $desiredParams);
-                if ($key !== false) {
-                    unset($desiredParams[$key]);
-                } else {
-                    $desiredParams[] = "pm25";
-                }
-                ?>
-                drawCharts();
-            });
-
-            $('#pm25s').click(function (e) {
-                e.preventDefault();
-                <?php
-                $key = array_search("pm25", $desiredParams);
-                if ($key !== false) {
-                    unset($desiredParams[$key]);
-                } else {
-                    $desiredParams[] = "pm25";
-                }
-                ?>
-                drawCharts();
-            });
-
-            $('#pm100s').click(function (e) {
-                e.preventDefault();
-                <?php
-                $key = array_search("pm100", $desiredParams);
-                if ($key !== false) {
-                    unset($desiredParams[$key]);
-                } else {
-                    $desiredParams[] = "pm100";
-                }
-                ?>
-                drawCharts();
-            });
-
-            $('#humi').click(function (e) {
-                e.preventDefault();
-                <?php
-                $key = array_search("humidity", $desiredParams);
-                if ($key !== false) {
-                    unset($desiredParams[$key]);
-                } else {
-                    $desiredParams[] = "humidity";
-                }
-                ?>
-                drawCharts();
-            });
-
-            $("#addBtn").click(function() {
-
-            });
         });
     </script>
 </head>
 
 <body>
-    <div id="menu" style="display: flex">
-        <div id="menuMain" style="flex: 1">
-            <input type="radio" id="day" name="period" value="day" checked onclick="drawCharts()">
-            <label for="day">Day</label>
+<div id="menu" style="display: flex">
+    <div id="menuMain" style="flex: 1">
+        <input type="radio" id="day" name="period" value="day" checked onclick="drawCharts()">
+        <label for="day">Day</label>
 
-            <input type="radio" id="week" name="period" value="week" onclick="drawCharts()">
-            <label for="week">Week</label>
+        <input type="radio" id="week" name="period" value="week" onclick="drawCharts()">
+        <label for="week">Week</label>
 
-            <input type="radio" id="month" name="period" value="month" onclick="drawCharts()">
-            <label for="month">Month</label>
+        <input type="radio" id="month" name="period" value="month" onclick="drawCharts()">
+        <label for="month">Month</label>
 
-            <input type="radio" id="fromUntil" name="period" value="fromUntil" onclick="drawCharts()">
-            <label for="month">From-Until</label>
-        </div>
+        <input type="radio" id="fromUntil" name="period" value="fromUntil" onclick="drawCharts()">
+        <label for="month">From-Until</label>
+    </div>
 
-        <div style="flex: 1;">
-            <canvas id="compareShow1" style="width: 15px; height: 15px; background: red"></canvas>
-            <button id="compareBtn">Compare Mode</button>
-            <canvas id="compareShow2" style="width: 15px; height: 15px; background: red"></canvas>
-            <br>
-            <button>Turn off compare mode</button>
+    <div id="compareMenu" style="flex: 1;">
+        <canvas id="compareShow1" style="width: 15px; height: 15px; background: red"></canvas>
+        <button id="compareBtn">Compare Mode</button>
+        <canvas id="compareShow2" style="width: 15px; height: 15px; background: red"></canvas>
+        <br>
+        <div id="popCompareMenu" style="display:none;">
+            <button id="clearCompareBtn">Turn off compare mode</button>
+            <div id="sensorSelection">
+                <button id="addSensorBtn">Add Sensor</button>
+                <input type="number" name="sensorIndex" id="sensorIndex" min="0">
+            </div>
             <div id="compareDiv">
                 <input type="checkbox" id="temp" name="temp" value="Temperature">
-                <label for="vehicle1">Temperature</label><br>
+                <label for="temp">Temperature</label><br>
                 <input type="checkbox" id="pres" name="pres" value="Pressure">
-                <label for="vehicle2">Pressure</label><br>
-                <input type="checkbox" id="pm25s" name="pm25s" value="PM2.5">
-                <label for="vehicle3">PM2.5</label><br>
-                <input type="checkbox" id="pm100s" name="pm100s" value="PM10">
-                <label for="vehicle1">PM10</label><br>
+                <label for="pres">Pressure</label><br>
+                <input type="checkbox" id="pm25s" name="pm25s" value="PM25">
+                <label for="pm25s">PM2.5</label><br>
+                <input type="checkbox" id="pm100s" name="pm100s" value="PM100">
+                <label for="pm100s">PM10</label><br>
                 <input type="checkbox" id="humi" name="humi" value="Humidity">
-                <label for="vehicle2">Humidity</label><br>
+                <label for="humi">Humidity</label><br>
             </div>
+            <button id="compareSubmit">Compare</button>
         </div>
-
-        <div id="rangeDiv">
-            <form id="sensor-data-form" onsubmit="event.preventDefault()";/>
-            <label for="start-date">From date:</label>
-            <input type="date" id="start-date" name="end-date">
-            <br>
-            <label for="end-date">Until date:</label>
-            <input type="date" id="end-date" name="end-date">
-            <br>
-            <input type="submit" value="Submit">
-            <button id="clearBtn">Clear</button>
-            </form>
-        </div>
-
     </div>
 
-<?php foreach ($sensors as $sensor):?>
-    <div class="sensor-row" style="outline: 2px solid royalblue; padding-top: 30px;padding-bottom: 20px;">
-        <h3 style="font-size: x-large; display: flex; justify-content: center"><?php $sql = "SELECT city, address, country FROM sensor_data WHERE sensor_id='" . $sensor . "' LIMIT 1";
-            $address = $db->query($sql);
-            $address = $address->fetch(PDO::FETCH_ASSOC);
-            echo $address["city"] . " " . $address["address"] . ", " . $address["country"] ?></h3>
-        <?php foreach (['temperature', 'humidity', 'pressure', 'pm25', 'pm100'] as $param): ?>
-            <div class="chartDiv" id="chart_<?php echo $sensor . "_" . $param; ?>" style="z-index: 3; border-radius: 25px; height: 35%; padding: 1px; margin-bottom: 60px; background: #ffffff; box-shadow: 0 0 40px rgb(59,162,246);"></div>
-        <?php endforeach;?>
+    <div id="rangeDiv">
+        <form id="sensor-data-form" onsubmit="event.preventDefault()" ;/>
+        <label for="start-date">From date:</label>
+        <input type="date" id="start-date" name="end-date">
+        <br>
+        <label for="end-date">Until date:</label>
+        <input type="date" id="end-date" name="end-date">
+        <br>
+        <input type="submit" value="Submit">
+        <button id="clearBtn">Clear</button>
+        </form>
     </div>
-    <div style="clear: both;"></div>
-<?php endforeach; ?>
+
+</div>
+<div id="defaultGraphsDiv">
+    <?php foreach ($sensors as $sensor): ?>
+        <div class="sensor-row" style="outline: 2px solid royalblue; padding-top: 30px;padding-bottom: 20px;">
+            <h3 style="font-size: x-large; display: flex; justify-content: center"><?php $sql = "SELECT city, address, country FROM sensor_data WHERE sensor_id='" . $sensor . "' LIMIT 1";
+                $address = $db->query($sql);
+                $address = $address->fetch(PDO::FETCH_ASSOC);
+                echo $address["city"] . " " . $address["address"] . ", " . $address["country"] ?></h3>
+            <?php foreach (['temperature', 'humidity', 'pressure', 'pm25', 'pm100'] as $param): ?>
+                <div class="chartDiv" id="chart_<?php echo $sensor . "_" . $param; ?>"
+                     style="z-index: 3; border-radius: 25px; height: 35%; padding: 1px; margin-bottom: 60px; background: #ffffff; box-shadow: 0 0 40px rgb(59,162,246);"></div>
+            <?php endforeach; ?>
+        </div>
+        <div style="clear: both;"></div>
+    <?php endforeach; ?>
+</div>
+
+
+<div id="compareGraphsDiv" style="display: none">
+    <div class="chartDiv" id="chart_temperature"
+         style="display:none; z-index: 3; border-radius: 25px; height: 35%; padding: 1px; margin-bottom: 60px; background: #ffffff; box-shadow: 0 0 40px rgb(59,162,246);"></div>
+    <div class="chartDiv" id="chart_pressure"
+         style="display:none; z-index: 3; border-radius: 25px; height: 35%; padding: 1px; margin-bottom: 60px; background: #ffffff; box-shadow: 0 0 40px rgb(59,162,246);"></div>
+    <div class="chartDiv" id="chart_pm25"
+         style="display:none; z-index: 3; border-radius: 25px; height: 35%; padding: 1px; margin-bottom: 60px; background: #ffffff; box-shadow: 0 0 40px rgb(59,162,246);"></div>
+    <div class="chartDiv" id="chart_pm100"
+         style="display:none; z-index: 3; border-radius: 25px; height: 35%; padding: 1px; margin-bottom: 60px; background: #ffffff; box-shadow: 0 0 40px rgb(59,162,246);"></div>
+    <div class="chartDiv" id="chart_humidity"
+         style="display:none; z-index: 3; border-radius: 25px; height: 35%; padding: 1px; margin-bottom: 60px; background: #ffffff; box-shadow: 0 0 40px rgb(59,162,246);"></div>
+</div>
+
 </body>
 </html>
